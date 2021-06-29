e772cff2f717ba1e1f8859875ad0698e77c0a43b
Aditya Vajjha <avajjha@amazon.com>
2021-06-29 14:29:11 +0800

    Changed AWS WAFv2 to Private WAFv2

---
+++ src/main/java/software/amazon/wafv2/loggingconfiguration/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.wafv2.loggingconfiguration;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("private-wafv2-loggingconfiguration.json");
+    }
+}
